a69ede52e3ed53f4b9c5f6bf8e165f4508f078b2
Kyllingene <fuzzymuffin@gmail.com>
2024-07-07 14:29:34 -0700

    hrmm

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,120 @@
+use circular_buffer::CircularBuffer;
+use cod::prelude::*;
 use errata::FallibleExt;
+use size_format::SizeFormatterSI;
 
-use std::process::Command;
-
-sarge::sarge! {
-    Args,
-
-    version: bool,
-}
+use std::process::{Stdio, Command};
+use std::io::{BufRead, BufReader};
 
 #[errata::catch]
 fn main() {
-    let (args, files) = Args::parse().fail("invalid arguments");
     let version = get_version();
 
-    if args.version {
-        println!("ddr version {}", env!("CARGO_PKG_VERSION"));
-        println!("dd version {}.{}", version.0, version.1);
+    let mut dd = Command::new("dd");
+    dd.arg("status=progress")
+        .stdin(Stdio::null())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped());
 
-        return;
+    let mut i = 0;
+    let mut infile = None;
+    let mut outfile = None;
+    for arg in std::env::args().skip(1) {
+        if i < 3 && arg == "--version" {
+            println!("ddr version {}", env!("CARGO_PKG_VERSION"));
+            println!("dd version {}.{}", version.0, version.1);
+            return Default::default(); // ok exit status
+        }
+
+        match i {
+            0 => {
+                dd.arg(format!("if={arg}"));
+                infile = Some(arg);
+            }
+            1 => {
+                dd.arg(format!("of={arg}"));
+                outfile = Some(arg);
+            }
+            2 => {
+                if arg != "--" {
+                    errata::error!("too many arguments (to pass flags to dd, use `--`)");
+                }
+            }
+            _ => { dd.arg(arg); }
+        }
+
+        i += 1;
     }
 
-    let [infile, outfile] = &files[1..] else {
-        errata::error!("expected at least two arguments, got {}", files.len() - 1);
-    };
+    if i < 2 {
+        errata::error!("expected at least 2 arguments, got {i}");
+    }
 
+    let infile = infile.unwrap();
+    let outfile = outfile.unwrap();
     println!("transferring `{infile}` to `{outfile}`");
 
-    let mut dd = Command::new("dd")
-        .arg(format!("if={infile}"))
-        .arg(format!("of={outfile}"))
-        .arg("progress=status");
+    let total_bytes = std::fs::metadata(infile).fail("failed to get length of infile").len();
+    let total_size = SizeFormatterSI::new(total_bytes);
+
+    let mut dd = dd.spawn().fail("failed to spawn dd");
+    std::thread::sleep(std::time::Duration::from_secs(1));
+    if let Ok(status) = dd.try_wait() {
+        std::io::copy(dd.stderr.as_mut().unwrap(), &mut std::io::stdout())
+            .fail("failed to read dd's stderr");
+
+        return status.unwrap_or_default();
+    }
+
+    let mut other_lines = CircularBuffer::<5, String>::new();
+    for line in BufReader::new(dd.stderr.take().unwrap()).lines() {
+        let Ok(line) = line else {
+            color::fg(3);
+            println!("failed to read output from dd");
+            color::de::fg();
+            continue;
+        };
+
+        if let Some(progress) = parse_progress(&line) {
+            goto::up(2);
+            color::fg(6);
+            cod::println!(
+                " {} / {} ({}%)\n {} secs, {}",
+                SizeFormatterSI::new(progress.bytes),
+                total_size,
+                progress.bytes as f64 / total_bytes as f64,
+                progress.elapsed_secs,
+                progress.throughput,
+            );
+            color::de::fg();
+        } else {
+            other_lines.push_back(line);
+        }
+
+        for line in &other_lines {
+            cod::println!("{line}");
+        }
+
+        goto::up(other_lines.len() as u32);
+
+        cod::flush();
+    }
+
+    dd.wait().fail("failed to spawn dd")
 }
 
 #[derive(Debug, Clone, Copy)]
 struct Progress<'a> {
     bytes: u64,
     elapsed_secs: f32,
-    bandwidth: &'a str,
+    throughput: &'a str,
 }
 
 fn parse_progress(line: &str) -> Option<Progress<'_>> {
     let bytes = line.split_once(' ')?.0.parse().ok()?;
-    let (rest, bandwidth) = {
-        let (r, b) = line.rsplit_once(',')?;
-        (r.trim(), b.trim())
+    let (rest, throughput) = {
+        let (r, t) = line.rsplit_once(',')?;
+        (r.trim(), t.trim())
     };
 
     let elapsed_secs = rest.rsplit_once(',')?.1.trim().strip_suffix(" s")?.parse().ok()?;
@@ -51,7 +122,7 @@ fn parse_progress(line: &str) -> Option<Progress<'_>> {
     Some(Progress {
         bytes,
         elapsed_secs,
-        bandwidth,
+        throughput,
     })
 }
 
